ec9794f7a57102216cb28743b4f3c8037f2f8baa
Pavan Reddy <66174691+pavanreddy09@users.noreply.github.com>
2021-09-03 11:29:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
+// const menu = document.querySelector(".menu");
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    showcase.classList.toggle('active');
+})
